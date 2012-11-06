f0c729d666806715888472c9993008cd2a2e90c8
Ricardo Ramirez <ricardo@bledo.co>
2012-11-05 23:08:56 -0500

    added script to deploy to github maven repository

---
+++ mvn-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+p=$1
+
+if [ "$p" = "" ]
+then
+	echo ""
+	echo "Path to cloned copy of https://github.com/bledo/maven2"
+	echo ""
+	exit
+fi
+
+rp=`readlink -f $p`
+
+echo mvn deploy -DaltDeploymentRepository=mine::default::file://$rp
+
